951773ba1f615db5f467b86af1ccc025ec928d1d
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-12-28 00:25:50 +0900

    init

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <router-view/>
   </main>
 </template>
+
+<style scoped>
+
+</style>
